ba15be2379f9e14c314f634c7a4e1cd286a983b9
Tanguy Gauvrit <tanguygauvrit@gmail.com>
2015-03-19 16:22:16 +0100

    Window sizing minimum fix

--- srcs/handle_term.c
+++ srcs/handle_term.c
@@ -6,7 +6,7 @@
 /*   By: tgauvrit <tgauvrit@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/03/18 20:20:24 by tgauvrit          #+#    #+#             */
-/*   Updated: 2015/03/19 15:58:52 by tgauvrit         ###   ########.fr       */
+/*   Updated: 2015/03/19 17:45:57 by tgauvrit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,14 +27,13 @@ void		handle_term(t_env *env)
 	char	*output;
 	int		i;
 	int		x;
-	int		y;
 
-	if ((1 + (env->argc / (env->height - 1)))
-		* (env->col_width + 1) > env->width)
+	if ((((env->argc - 1) / (env->height - 1)) * (env->col_width + 1))
+		+ env->col_width > env->width)
 	{
 		ft_putstr_fd(tgoto(tgetstr("cm", NULL), 0, 0), tty_fd());
 		ft_putstr_fd(tgetstr("cd", NULL), tty_fd());
-		write(tty_fd(), "Increase window size...\n", 24);
+		write(tty_fd(), "Increase window size...", 23);
 		return ;
 	}
 	output = ft_strdup(tgoto(tgetstr("cm", NULL), 0, 0));
@@ -43,8 +42,8 @@ void		handle_term(t_env *env)
 	while (i < env->argc)
 	{
 		x = (i / (env->height - 1)) * (env->col_width + 1);
-		y = i % (env->height - 1);
-		ft_strjoinfree(&output, tgoto(tgetstr("cm", NULL), x, y));
+		ft_strjoinfree(&output, tgoto(tgetstr("cm", NULL), x
+			, i % (env->height - 1)));
 		arg_text(env, &output, i);
 		i++;
 	}
